09415194bb03cadfc46ca88fd698fcaec648000d
Anand Abhishek <45845267+anabhish@users.noreply.github.com>
2026-03-22 19:01:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 
 #load the training model
 
-model = tf.keras.models.load_model('model.keras', compile=False)
+#model = tf.keras.models.load_model('model.keras')
+model = tf.keras.models.load_model('model.h5')
 
 # load the encoders and scaler
 
